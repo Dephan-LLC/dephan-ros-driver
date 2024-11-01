fe546b27a0c1397faa8e7452fbaaeffb61848e8c
Alexander Plukchi <a.plukchi@dephan.com>
2024-11-01 11:07:54 +0300

    [fix] ^C correct handling in UDP mode

--- src/mech_lidar_driver/src/ros_driver.cpp
+++ src/mech_lidar_driver/src/ros_driver.cpp
@@ -79,7 +79,11 @@ void Driver::_poll_full_udp() {
         packet::raw_packet_t raw_pkt(new uint8_t[packet::PKT_LEN]);
 
         // wait until we are receive data
-        while (socket->get_packet(raw_pkt.get(), packet::PKT_LEN)) {}
+        while (socket->get_packet(raw_pkt.get(), packet::PKT_LEN)) {
+            if (!ros::ok()) {
+                return;
+            }
+        }
 
         // transform raw packet to handled packet
         pkt_hdl_Mech hdl_pkt(std::move(raw_pkt));
@@ -177,7 +181,11 @@ void Driver::_poll_udp() {
     packet::raw_packet_t raw_pkt(new uint8_t[packet::PKT_LEN]);
 
     // wait until we are receive data
-    while (socket->get_packet(raw_pkt.get(), packet::PKT_LEN)) {}
+    while (socket->get_packet(raw_pkt.get(), packet::PKT_LEN)) {
+        if (!ros::ok()) {
+            return;
+        }
+    }
 
     // transform raw packet to handled packet
     pkt_hdl_Mech hdl_pkt(std::move(raw_pkt));
